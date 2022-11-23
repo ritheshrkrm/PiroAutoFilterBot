39d1d5c22056357faf8fa79f94e44e34d58ca124
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2022-11-23 22:18:25 +0530

    fix

--- bot.py
+++ bot.py
@@ -21,8 +21,6 @@ from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 TIMEZONE = (os.environ.get("TIMEZONE", "Asia/Kolkata"))
 
-PORT = "8080"
-
 class Bot(Client):
 
     def __init__(self):
