7fb84afa5ccc1601f737686321b4314827535757
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2022-11-27 10:49:53 +0530

    fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -771,7 +771,9 @@ async def auto_filter(client, msg, spoll=False):
                 await asyncio.sleep(300)
                 await fek.delete()
     else:
-        await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+        fuk = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+        await asyncio.sleep(300)
+        await fuk.delete()        
     if spoll:
         await msg.message.delete()
 
