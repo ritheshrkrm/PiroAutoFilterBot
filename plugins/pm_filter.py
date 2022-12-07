5cced0d206eecec14ead1facc199cd4140eb7d4d
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2022-12-07 16:36:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -34,7 +34,7 @@ logger.setLevel(logging.ERROR)
 BUTTONS = {}
 SPELL_CHECK = {}
 
-@Client.on_message((filters.group | filters.private) & filters.text & filters.incoming)
+@Client.on_message((filters.group & filters.text & filters.incoming & filters.text & filters.incoming)
 async def give_filter(client, message):
     k = await global_filters(client, message)
     if k == False:
