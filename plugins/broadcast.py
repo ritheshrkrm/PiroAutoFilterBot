83c1fbd6aa883ed7868189ef73be3e4a03a22251
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2022-12-08 21:16:08 +0530

    fix

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -6,6 +6,7 @@ import datetime
 import time
 from database.users_chats_db import db
 from info import ADMINS
+from utils import broadcast_messages, broadcast_messages_group
 import asyncio
         
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
