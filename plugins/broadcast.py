3503784974ff9a900079ee56370aa9f2921deb69
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2022-12-08 20:00:24 +0530

    updated command

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -42,7 +42,7 @@ async def verupikkals(bot, message):
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
 
-@Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
+@Client.on_message(filters.command("group_broadcast") & filters.user(ADMINS) & filters.reply)
 async def grp_brodcst(bot, message):
     chats = await db.get_all_chats()
     b_msg = message.reply_to_message
