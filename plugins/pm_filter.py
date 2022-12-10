6a013a75005c0dc06131e465e7d1884e5b0247c2
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2022-12-10 16:44:45 +0530

    fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -102,7 +102,7 @@ async def next_page(bot, query):
         [
             InlineKeyboardButton(f'📂 𝖥𝗂𝗅𝖾𝗌: {len(files)}', 'dupe'),
             InlineKeyboardButton(f'📮 𝖨𝗇𝖿𝗈', 'info'),
-            InlineKeyboardButton(text=f"{imdb.get('title')}",url=imdb['url'])
+            InlineKeyboardButton(f'🔗 𝖨𝖬𝖣𝖻', 'info')
         ]
     )
 
@@ -718,7 +718,7 @@ async def auto_filter(client, msg, spoll=False):
         [
             InlineKeyboardButton(f'📂 𝖥𝗂𝗅𝖾𝗌: {len(files)}', 'dupe'),
             InlineKeyboardButton(f'📮 𝖨𝗇𝖿𝗈', 'info'),
-            InlineKeyboardButton(text=f"{imdb.get('title')}",url=imdb['url'])
+            InlineKeyboardButton(f'🔗 𝖨𝖬𝖣𝖻', 'info')
         ]
     )    
 
