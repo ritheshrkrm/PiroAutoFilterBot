36c56b86dd2eb20ddc6160baf5ab52cc1a72fd2c
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2022-12-13 20:46:38 +0530

    fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -102,7 +102,7 @@ async def next_page(bot, query):
     btn.insert(1,
         [
             InlineKeyboardButton(f'📝 𝖳𝗂𝗉𝗌', 'info'),
-            InlineKeyboardButton("🔎 𝖦𝗈𝗈𝗀𝗅𝖾", url=f"{imdb.get('url')}")
+            InlineKeyboardButton("🔎 𝖦𝗈𝗈𝗀𝗅𝖾", url=f"{url}")
         ]
     )
 
@@ -712,7 +712,7 @@ async def auto_filter(client, msg, spoll=False):
     btn.insert(1,
         [
             InlineKeyboardButton(f'📝 𝖳𝗂𝗉𝗌', 'info'),
-            InlineKeyboardButton("🔎 𝖦𝗈𝗈𝗀𝗅𝖾", url=f"{imdb.get('url')}")
+            InlineKeyboardButton("🔎 𝖦𝗈𝗈𝗀𝗅𝖾", url=f"{url}")
         ]
     )    
 
