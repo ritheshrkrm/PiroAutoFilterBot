f7af45bbd6951bc2ce917593646af6d5e05efe28
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2022-12-21 19:09:27 +0530

    fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -35,14 +35,14 @@ logger.setLevel(logging.ERROR)
 BUTTONS = {}
 SPELL_CHECK = {}
 
-@Client.on_message(filters.group & filters.text & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     await global_filters(client, message)
     mf = await manual_filters(client, message)
     if mf == False:
         await auto_filter(client, message)
 
-@Client.on_message(filters.private & filters.text & filters.incoming)
+@Client.on_message(filters.private & filters.text & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.private & filters.text & filters.incoming)
 async def pv_filter(client, message):
     kd = await global_filters(client, message)
     if kd == False:
