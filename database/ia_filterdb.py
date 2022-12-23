fd3148a1ef88567b6f2df3535e64adffe71be626
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2022-12-23 23:09:37 +0530

    fix

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -66,9 +66,8 @@ async def save_file(media):
 
 
 
-async def get_search_results(query, file_type=None, max_results=10, offset=0, filter=False):
+async def get_search_results(chat_id, query, file_type=None, max_results=5, offset=0, filter=False):
     """For given query return (results, next_offset)"""
-
     query = query.strip()
     #if filter:
         #better ?
@@ -110,7 +109,6 @@ async def get_search_results(query, file_type=None, max_results=10, offset=0, fi
 
     return files, next_offset, total_results
 
-
 async def get_bad_files(query, file_type=None, max_results=100, offset=0, filter=False):
     """For given query return (results, next_offset)"""
     query = query.strip()
@@ -124,7 +122,7 @@ async def get_bad_files(query, file_type=None, max_results=100, offset=0, filter
         raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
         raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
-
+    
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
     except:
