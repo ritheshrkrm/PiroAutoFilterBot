95efa201691f1985c47b007f4ec7e49a3281bd5b
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2022-12-25 08:07:47 +0530

    bug fixes

--- utils.py
+++ utils.py
@@ -129,7 +129,6 @@ async def get_poster(query, bulk=False, id=False, file=None):
         'rating': str(movie.get("rating")),
         'url':f'https://www.imdb.com/title/tt{movieid}'
     }
-# https://github.com/odysseusmax/animated-lamp/blob/2ef4730eb2b5f0596ed6d03e7b05243d93e3415b/bot/utils/broadcast.py#L37
 
 async def broadcast_messages(user_id, message):
     try:
