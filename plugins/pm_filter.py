38e1fb5cca6e0bb6af6601c0e1bf818b3618bee1
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2023-01-10 16:05:43 +0530

    PM support added

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -54,17 +54,10 @@ async def give_filter(client, message):
                 await auto_filter(client, message) 
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
-async def pm_text(bot, message):
-    content = message.text
-    user = message.from_user.first_name
-    user_id = message.from_user.id
-    if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
-    if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>𝖸𝗈𝗎𝗋 𝖬𝖾𝗌𝗌𝖺𝗀𝖾 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖲𝖾𝗇𝗍 𝖳𝗈 𝖬𝗈𝖽𝖾𝗋𝖺𝗍𝗈𝗋 !</b>")
-    await bot.send_message(
-        chat_id=LOG_CHANNEL,
-        text=f"<b>#PM Message\n\nName : {user}\n\nID : {user_id}\n\nMessage : {content}</b>"
-    )
+async def pv_filter(client, message):
+    kd = await global_filters(client, message)
+    if kd == False:
+        await auto_filter(client, message)
 
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
