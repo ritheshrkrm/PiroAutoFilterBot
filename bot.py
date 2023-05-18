d2451806068bce9faf8cb820b107855add9462b8
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2023-05-18 21:36:52 +0530

    restart message on support group

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ class Bot(Client):
         now = datetime.now(tz)
         time = now.strftime("%H:%M:%S %p")
         await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
-        await self.send_message(chat_id=SUPPORT_CHAT_ID, text=script.RESTART_TXT.format(today, time))
+        await self.send_message(chat_id=SUPPORT_CHAT_ID, text=script.RESTART_GC_TXT.format(today, time))
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
