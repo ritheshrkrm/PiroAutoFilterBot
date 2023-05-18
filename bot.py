e49f883d5db34efd6c20692ca23cef5100623d4a
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2023-05-18 22:06:05 +0530

    fix

--- bot.py
+++ bot.py
@@ -17,6 +17,8 @@ from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from Script import script 
 from datetime import date, datetime 
+import pytz
+
 
 class Bot(Client):
 
