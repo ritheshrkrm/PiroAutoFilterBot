aafe0e1495f6bb9fa76abd2306c3d02eea66258c
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2023-06-23 19:38:10 +0530

    up

--- plugins/approve.py
+++ plugins/approve.py
@@ -5,11 +5,15 @@ from pyrogram.types import Message, User, ChatJoinRequest
 from info import TEXT, APPROVED 
 
 
-@Client.on_chat_join_request((filters.group | filters.channel) & filters.chat)
-async def autoapprove(client, message: ChatJoinRequest):
-    chat=message.chat 
-    user=message.from_user 
-    print(f"{user.first_name} Joined (Approved)") 
+@ritheshpiro17.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+async def autoapprove(client: ritheshpiro17, message: ChatJoinRequest):
+    chat=message.chat # Chat
+    user=message.from_user # User
+    print(f"{user.first_name} Joined (Approved)") # Logs
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
         await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+        print("𝖧𝖾𝗅𝗅𝗈 {mention}\n𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝖳𝗈 {title}\n\n𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖠𝗉𝗉𝗋𝗈𝗏𝖾𝖽...!!!")
+
+print("Auto Approved Bot")
+ritheshpiro17.run()
