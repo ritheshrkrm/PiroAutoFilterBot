75178432a7f90dda1c85e53027d82661d5e00f38
『ᴘɪʀᴏ』 <115382959+ritheshrkrm@users.noreply.github.com>
2023-06-24 16:21:19 +0530

    experimental try

--- plugins/check_alive.py
+++ plugins/check_alive.py
@@ -1,6 +1,9 @@
-import time
 import random
-from pyrogram import Client, filters
+import re, asyncio, time, shutil, psutil, os, sys
+from pyrogram import Client, filters, enums
+from pyrogram.types import *
+from info import BOT_START_TIME, ADMINS
+from utils import humanbytes  
 
 CMD = ["/", "."]
 
@@ -32,3 +35,33 @@ async def ping(_, message):
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
     await rm.edit(f"𝖯𝗂𝗇𝗀!\n{time_taken_s:.3f} ms")
+
+@Client.on_message(filters.private & filters.command("status") & filters.user(ADMINS))          
+async def stats(bot, update):
+    currentTime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - BOT_START_TIME))
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+
+    ms_g = f"""<b>𝖡𝗈𝗍 𝖲𝗍𝖺𝗍𝗎𝗌</b>
+
+𝖴𝗉𝗍𝗂𝗆𝖾: <code>{currentTime}</code>
+𝖢𝖯𝖴 𝖴𝗌𝖺𝗀𝖾: <code>{cpu_usage}%</code>
+𝖱𝖠𝖬 𝖴𝗌𝖺𝗀𝖾: <code>{ram_usage}%</code>
+𝖳𝗈𝗍𝖺𝗅 𝖣𝗂𝗌𝗄 𝖲𝗉𝖺𝖼𝖾: <code>{total}</code>
+𝖴𝗌𝖾𝖽 𝖲𝗉𝖺𝖼𝖾: <code>{used} ({disk_usage}%)</code>
+𝖥𝗋𝖾𝖾 𝖲𝗉𝖺𝖼𝖾: <code>{free}</code> """
+
+    msg = await bot.send_message(chat_id=update.chat.id, text="__𝖯𝗋𝗈𝖼𝖾𝗌𝗌𝗂𝗇𝗀...__", parse_mode=enums.ParseMode.MARKDOWN)         
+    await msg.edit_text(text=ms_g, parse_mode=enums.ParseMode.HTML)
+
+@Client.on_message(filters.command("restart") & filters.user(ADMINS))
+async def stop_button(bot, message):
+    msg = await bot.send_message(text="**𝖡𝗈𝗍 𝖨𝗌 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝗂𝗇𝗀...**", chat_id=message.chat.id)       
+    await asyncio.sleep(3)
+    await msg.edit("**𝖡𝗈𝗍 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝖾𝖽 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅𝗅𝗒 ! 𝖱𝖾𝖺𝖽𝗒 𝖳𝗈 𝖬𝗈𝗏𝖾 𝖮𝗇 🫡**")
+    os.execl(sys.executable, sys.executable, *sys.argv)
