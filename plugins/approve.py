e10a739cc60c63d50c69a00def7205b40e7ed9f8
Manu <manukrishnaos45@gmail.com>
2023-06-25 17:38:35 +0530

    req-ack

--- plugins/approve.py
+++ plugins/approve.py
@@ -1,23 +1,15 @@
-from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from Script import script
-from info import REQ_PICS
-from utils import temp
-import random
-
+from pyrogram import Client
 
 @Client.on_chat_join_request()
 async def accept_request(client, r):
-buttons = [[
-    InlineKeyboardButton("❤️‍🔥 𝖡𝖫𝖠𝖲𝖳𝖤𝖱 𝖧𝖴𝖡 ❤️‍🔥", url=f"https://https://t.me/blaster_hub"),
-    InlineKeyboardButton("⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡", url=f"https://t.me/piroxbots")
-]]
     
-    await client.send_photo(
-        r.from_user.id,
-        random.choice(REQ_PICS),
-        script.REQ_TXT.format(r.from_user.mention, temp.U_NAME, temp.B_NAME),
-        enums.ParseMode.HTML,
-        reply_markup = InlineKeyboardMarkup(buttons))
+    try:
+        await client.send_message(r.from_user.id,
+            f"Hello {r.from_user.mention}\nWelcome to {r.chat.title}\nYou Request Have Been Accepted...!!!")
+
+    except UserIsBlocked:
+        print("User blocked the bot")
+    except PeerIdInvalid:
+        print("Err")
 
     await r.approve()
